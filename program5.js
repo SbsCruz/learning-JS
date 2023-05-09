d227f7b88e55ea468032b5c7bf88bdfced9cc8e3
SbsCruz <sebas.cruz750@gmail.com>
2023-05-09 11:41:05 -0500

    cap-5

---
+++ program5.js
@@ -0,0 +1,74 @@
+//-------------CONSOLA-------------//
+/*
+
+// assert()
+// muestra un mensaje de error  solo si el valor 
+// de verdad es falso
+//console.assert(5<4)
+
+// clear()
+// se limpia la consola
+// console.clear()
+
+// error()
+// muestra un mensaje predefinido para algún error
+//console.error("No hagas eso!!!")
+
+// info()
+// muestra un mensaje predefinido para información
+// console.info("información olvidada")
+
+// log()
+// es un mensaje de depuración
+// se usa en lugar de INFO
+
+// table()
+// genera una tabla con el array o el objeto ingresado
+// console.table([1,2,3,1,2,3,1,2,3])
+
+// dir()
+// muestra el contenido de un array u objeto
+// de forma interactiva
+// console.dir([1,2,3,1,2,3,1,2,3])
+*/
+
+//-------------METODOS DE CONTEO-------------//
+/*
+// count()
+// registra el número de veces que se hace uso
+// de esta función
+// console.count()
+// console.count()
+// console.count()
+
+
+// countReset()
+// resetea el contador de count()
+// console.countReset()
+// console.count()
+*/
+
+//-------------METODOS DE AGRUPAMIENTO-------------//
+/*
+// group()
+// se crean grupos en la consola
+// se puede definir el nombre
+
+// groupEnd()
+// empezamos a escribir en el grupo anterior
+
+// groupCollapsed()
+// se crea un grupo "cerrado"
+*/
+
+//-------------METODOS DE TEMPORIZACIÓN-------------//
+
+// time()
+// inica un contador
+
+// timeLog()
+// indica el tiempo que pasó desde que se 
+// ejecutó el contador
+
+
+
